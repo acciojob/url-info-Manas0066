5c37931bad8d01a3e7f5feacb39621381d220256
Manas Chaudhary <manaschaudhary1602@gmail.com>
2025-09-05 04:21:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+    // get current URL
+    let currentURL = location.href;
+
+    // get its length
+    let urlLength = currentURL.length;
+
+    // show result
+    alert("The length of the URL is: " + urlLength);
+ 
